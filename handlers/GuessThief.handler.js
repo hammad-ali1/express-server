07834a640bf94476e66c1e704471ab899601968c
Hammad Ali <hammad.a22002@gmail.com>
2022-08-04 21:24:15 +0500

    removed update-round socket event

--- handlers/GuessThief.handler.js
+++ handlers/GuessThief.handler.js
@@ -3,9 +3,8 @@ module.exports = function (io, socket) {
     io.to(roomId).emit("refresh-cards", { newCards });
   });
 
-  socket.on("start-GuessThief", ({ roomId, roundLimit }) => {
-    console.log("round limit received: " + roundLimit);
-    io.to(roomId).emit("start-GuessThief", { roundLimit });
+  socket.on("start-GuessThief", ({ roomId }) => {
+    io.to(roomId).emit("start-GuessThief");
   });
 
   socket.on("update-points", ({ newPoints, roomId }) => {
@@ -24,9 +23,6 @@ module.exports = function (io, socket) {
   socket.on("open-snackbar", ({ message, roomId }) => {
     io.to(roomId).emit("open-snackbar", { message });
   });
-  socket.on("update-round-limit", ({ roomId, newRoundLimit }) => {
-    console.log("updating round limit to: " + newRoundLimit);
-    io.to(roomId).emit("update-round-limit", { newRoundLimit });
-  });
+
   return {};
 };
